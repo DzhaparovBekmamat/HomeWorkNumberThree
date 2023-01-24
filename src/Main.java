98d3d62031ab54005e8bdae07ba339332c6e3e97
Dzhaparov Bekmamat Kanybekovich <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-01-24 13:46:58 +0600

    Update Main.java
    Исправил некоторые моменты по массиву

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         // Д/З
         double[] digits = {9.9, -0.5, 11.1, 2.5, -4.99, 5.32, -0.99, 2.2, -5.0, 9.11, 0.1, 10.00, -3.9, 3.33, -6.8};
-        double[] digits2 = {11.1, 2.5, 5.32, 2.2, 9.11, 0.1, 10.00, 3.33};
         double result = 0;
-        for (double division : digits2) {
+        double[] numbers = {2,3,4,5,6,7,8,9,10,11,12,13};
+        for (double division : numbers) {
             result += division;
         }
         System.out.println("Среднее арифметичсекое всех чисел равно = " + result);
