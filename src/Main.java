2723a2eccb9fe7df924b7aeb729882851050160e
Dzhaparov Bekmamat Kanybekovich <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-01-26 11:29:12 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        // Д/З
-        double[] digits = {9.9, -0.5, 11.1, 2.5, -4.99, 5.32, -0.99, 2.2, -5.0, 9.11, 0.1, 10.00, -3.9, 3.33, -6.8};
+        double[] numbers ={9.9, -0.5, 11.1, 2.5, -4.99, 5.32, -0.99, 2.2, -5.0, 9.11, 0.1, 10.00, -3.9, 3.33, -6.8};
         double result = 0;
-        double[] numbers = {2,3,4,5,6,7,8,9,10,11,12,13};
-        for (double division : numbers) {
-            result += division;
+        int quantity = 0;
+        boolean foundNegative = false;
+        for (double digits : numbers) {
+            if (digits < 0) {
+                foundNegative = true;
+            } else if (foundNegative) {
+                result += digits;
+                quantity++;
+            }
         }
-        System.out.println("Среднее арифметичсекое всех чисел равно = " + result);
-        System.out.println();
-       }
+        double mean = result / quantity;
+        System.out.println("Среднее арифметическое положительных чисел, расположенных после первого отрицательного числа = " + mean);
+    }
 }
+
